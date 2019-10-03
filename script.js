099803a23111f8e3398df48a56e4930455ea62e1
tamba91 <tamba.1991@hotmail.it>
2019-10-03 13:04:47 +0200

    ajax added

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("prova2");
+var xhr = new XMLHttpRequest();
+
+xhr.onload = function () {
+    if (this.status >= 200 && this.status < 400) {
+        var res = JSON.parse(this.responseText);
+        console.log(res[0]);
+    }
+}
+
+xhr.open("GET", "https://www.dati.lombardia.it/resource/nicp-bhqi.json");
+
+xhr.send();
