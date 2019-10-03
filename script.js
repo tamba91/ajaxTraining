f24ccd40d3e6f52231dd4888473b41ef7f594f30
tamba91 <tamba.1991@hotmail.it>
2019-10-03 22:29:06 +0200

    ajax init

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ var xhr = new XMLHttpRequest();
 xhr.onload = function () {
     if (this.status >= 200 && this.status < 400) {
         var res = JSON.parse(this.responseText);
-        console.log(res[0]);
+        for (var i = 0; i < res.length; i++) {
+            if (res[i].idsensore == 9938) {
+                document.getElementById("target").innerHTML = "temperatura: " + res[i].valore + "<br>" + "data e ora: " + res[i].data;
+            }
+
+        }
     }
 }
 
-xhr.open("GET", "https://www.dati.lombardia.it/resource/nicp-bhqi.json");
+xhr.open("GET", "https://www.dati.lombardia.it/resource/647i-nhxk.json", true);
 
 xhr.send();
