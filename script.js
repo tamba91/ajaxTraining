81923f16b4e0e81dd578cbf2aced2d20a13c925f
tamba91 <tamba.1991@hotmail.it>
2019-10-03 22:44:11 +0200

    ajax 9938 corretto

--- script.js
+++ script.js
@@ -4,14 +4,11 @@ xhr.onload = function () {
     if (this.status >= 200 && this.status < 400) {
         var res = JSON.parse(this.responseText);
         for (var i = 0; i < res.length; i++) {
-            if (res[i].idsensore == 9938) {
-                document.getElementById("target").innerHTML = "temperatura: " + res[i].valore + "<br>" + "data e ora: " + res[i].data;
-            }
-
+            document.getElementById("target").innerHTML += "temperatura: " + res[i].valore + " " + "data e ora: " + res[i].data + "<br>";
         }
     }
 }
 
-xhr.open("GET", "https://www.dati.lombardia.it/resource/647i-nhxk.json", true);
+xhr.open("GET", "https://www.dati.lombardia.it/resource/647i-nhxk.json?idsensore=9938", true);
 
 xhr.send();
