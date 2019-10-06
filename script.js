608eae7a1ab4fe25ecb049476270749565306853
tamba91 <tamba.1991@hotmail.it>
2019-10-06 15:53:42 +0200

    complementa aggiornata

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
-var xhr = new XMLHttpRequest();
-function formatDataOdierna(){
+function formatDataOdierna() {
     var data = new Date();
-    console.log(data.getUTCFullYear() + "-" + data.getUTCMonth() + "-" + data.getUTCDay() + "T");
-    return "'2019-10-03T23:00:00.000'";
+    return ("'" + data.getFullYear() + "-" + complementa(data.getMonth() + 1) + "-" + complementa(data.getUTCDate()) + "T00:00:00.000" + "'");
 }
 
-function complementa(valoreData){
-    if(valoreData >= 1 && valoreData <= 9){
+function complementa(valoreData) {
+    if (valoreData >= 1 && valoreData <= 9) {
         return "0" + valoreData;
     }
-    else if((valoreData >= 10 && valoreData <= 31)){
-        return toString(valoreData);
+    else if ((valoreData >= 10 && valoreData <= 31)) {
+        return "" + valoreData;
     }
 
-    else{
+    else {
         return "valore data non ammissibile"
     }
 }
 
 var data = formatDataOdierna();
 
+var xhr = new XMLHttpRequest();
 xhr.onload = function () {
     if (this.status >= 200 && this.status < 400) {
         var res = JSON.parse(this.responseText);
         for (var i = 0; i < res.length; i++) {
-            document.getElementById("target").innerHTML += "temperatura: " + res[i].valore + " " + "data e ora: " + res[i].data + "<br>";
+            var p = document.createElement("p");
+            var txt = document.createTextNode("temperatura: " + + res[i].valore + " " + "data e ora: " + res[i].data);
+            p.appendChild(txt);
+            document.body.appendChild(p);
+            //document.getElementById("target").innerHTML += "temperatura: " + res[i].valore + " " + "data e ora: " + res[i].data + "<br>";
         }
     }
 }
@@ -33,4 +36,5 @@ xhr.open("GET", "https://www.dati.lombardia.it/resource/647i-nhxk.json?idsensore
 
 xhr.send();
 
-console.log(complementa(7));
+console.log(typeof(complementa(7)));
+console.log(typeof(complementa(21)));
