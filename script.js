bf0381c4ffeada4576445092e4bbb137b7f4f986
tamba91 <tamba.1991@hotmail.it>
2019-10-31 22:02:44 +0100

    date setting not working

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-function formatDataOdierna() {
-    var data = new Date();
-    return ("'" + data.getFullYear() + "-" + complementa(data.getMonth() + 1) + "-" + complementa(data.getUTCDate()) + "T00:00:00.000" + "'");
+function formatData(data) {
+    return ("'" + data.getFullYear() + "-" + complementa(data.getMonth() + 1) + "-" + complementa(data.getDate()) + "T00:00:00.000" + "'");
 }
 
 function complementa(valoreData) {
@@ -16,24 +15,59 @@ function complementa(valoreData) {
     }
 }
 
-var compiled = _.template("<div class='row'><div class='one-half column'><h4>temperatura: <%= valore %></h4></div> <div class='one-half column'><h4>data e ora: <%= data %></h4></div></div>");
-var data = formatDataOdierna();
-
+var rowView = "<td>temperatura: <%= valore %>°</td><td>data e ora: <%= data %></td>";
+var compiled = _.template(rowView);
 var xhr = new XMLHttpRequest();
-xhr.onload = function () {
-    if (this.status >= 200 && this.status < 400) {
-        var res = JSON.parse(this.responseText);
-        for (var i = 0; i < res.length; i++) {
-            var riga = compiled(res[i]);
-            document.body.innerHTML += riga;
-            //document.getElementById("target").innerHTML += "temperatura: " + res[i].valore + " " + "data e ora: " + res[i].data + "<br>";
+var selezione = document.getElementById("selezione");
+var data = new Date();
+selezione.addEventListener("change", loadData);
+
+var radios = document.getElementsByName("giorno");
+
+for (var i = 0; i < radios.length; i++) {
+    radios[i].addEventListener("change", function () {
+        if (this.value == "0") {
+            data = Date(Date.now());
+            console.log(typeof(data));
         }
-    }
+        else if (this.value == "-1") {
+            data.setDate(data.getDate() - 1);
+            console.log(typeof(data));
+        }
+        console.log(data);
+    })
 }
 
-xhr.open("GET", "https://www.dati.lombardia.it/resource/647i-nhxk.json?idsensore=12025&$where=data>=" + data + "&$order=data DESC", true);
+var dataFormatted = formatData(data);
 
-xhr.send();
 
-console.log(typeof (complementa(7)));
-console.log(typeof (complementa(21)));
+function ajaxCall(url, callback) {
+    xhr.onload = function () {
+        if (this.status >= 200 && this.status < 400) {
+            callback(this.responseText);
+        }
+    }
+    xhr.open("GET", url);
+    xhr.send();
+}
+
+function loadData() {
+
+    if (selezione.value != "") {
+        ajaxCall("https://www.dati.lombardia.it/resource/647i-nhxk.json?idsensore=" + selezione.value + "&$where=data>=" + dataFormatted + "&$order=data ASC", function (res) {
+            var table = document.getElementById("data-table-body");
+            table.innerHTML = "";
+            var response = JSON.parse(res);
+            for (var i = 0; i < response.length; i++) {
+                var fTemplate = compiled(response[i]);
+
+
+                var row = table.insertRow(0);
+                row.innerHTML = fTemplate;
+            }
+        })
+    }
+    else {
+        document.getElementById("data-table-body").innerHTML = "";
+    }
+}
