b54458bb51575dc71967d6a5869388fc09e4166f
tamba91 <tamba.1991@hotmail.it>
2019-11-05 20:42:06 +0100

    05/11

--- script.js
+++ script.js
@@ -15,32 +15,6 @@ function complementa(valoreData) {
     }
 }
 
-var rowView = "<td>temperatura: <%= valore %>°</td><td>data e ora: <%= data %></td>";
-var compiled = _.template(rowView);
-var xhr = new XMLHttpRequest();
-var selezione = document.getElementById("selezione");
-var data = new Date();
-selezione.addEventListener("change", loadData);
-
-var radios = document.getElementsByName("giorno");
-
-for (var i = 0; i < radios.length; i++) {
-    radios[i].addEventListener("change", function () {
-        if (this.value == "0") {
-            data = new Date();
-            console.log(typeof(data));
-        }
-        else if (this.value == "-1") {
-            data.setDate(data.getDate() - 1);
-            console.log(typeof(data));
-        }
-        console.log(data);
-    })
-}
-
-var dataFormatted = formatData(data);
-
-
 function ajaxCall(url, callback) {
     xhr.onload = function () {
         if (this.status >= 200 && this.status < 400) {
@@ -52,22 +26,56 @@ function ajaxCall(url, callback) {
 }
 
 function loadData() {
-
+    document.getElementById("data-table-body").innerHTML = "";
     if (selezione.value != "") {
-        ajaxCall("https://www.dati.lombardia.it/resource/647i-nhxk.json?idsensore=" + selezione.value + "&$where=data>=" + dataFormatted + "&$order=data ASC", function (res) {
+        ajaxCall("https://www.dati.lombardia.it/resource/647i-nhxk.json?idsensore=" + selezione.value + "&$where=data>=" + dataFormatted + "&$order=data DESC", function (res) {
             var table = document.getElementById("data-table-body");
-            table.innerHTML = "";
             var response = JSON.parse(res);
             for (var i = 0; i < response.length; i++) {
                 var fTemplate = compiled(response[i]);
-
-
                 var row = table.insertRow(0);
                 row.innerHTML = fTemplate;
             }
         })
     }
-    else {
-        document.getElementById("data-table-body").innerHTML = "";
-    }
 }
+
+var rowView = "<td>temperatura: <%= valore %>°</td><td>data e ora: <%= data %></td>";
+var compiled = _.template(rowView);
+var xhr = new XMLHttpRequest();
+var selezione = document.getElementById("selezione");
+var radios = document.getElementsByName("giorno");
+var data = new Date();
+var dateStart = new Date();
+var dateEnd = new Date();
+dateStart.setDate(dateStart.getDate() - 1);
+dateEnd.setDate(dateEnd.getDate() + 1);
+console.log("yesterday-dateStart: ", dateStart);
+console.log("tomorrow-DateEnd: ", dateEnd);
+var dataFormatted = formatData(data);
+
+selezione.addEventListener("change", loadData);
+
+
+for (var i = 0; i < radios.length; i++) {
+    radios[i].addEventListener("change", function () {
+        if (this.value == "0") {
+            data = new Date();
+            dataFormatted = formatData(data);
+            loadData();
+            console.log(dataFormatted);
+        }
+        else if (this.value == "-1") {
+            data.setDate(data.getDate() - 1);
+            dataFormatted = formatData(data);
+            loadData();
+            console.log(dataFormatted);
+        }
+
+    })
+}
+
+
+
+
+
