b8f784d54887dd8f600596b5019a96ff55cf7e5b
tamba91 <tamba.1991@hotmail.it>
2019-11-09 21:52:29 +0100

    selezione date modificata per giorno

--- script.js
+++ script.js
@@ -25,10 +25,12 @@ function ajaxCall(url, callback) {
     xhr.send();
 }
 
-function loadData() {
+function loadData(dateStart, dateEnd) {
+    console.log(dateStart);
+    console.log(dateEnd);
     document.getElementById("data-table-body").innerHTML = "";
     if (selezione.value != "") {
-        ajaxCall("https://www.dati.lombardia.it/resource/647i-nhxk.json?idsensore=" + selezione.value + "&$where=data>=" + dataFormatted + "&$order=data DESC", function (res) {
+        ajaxCall("https://www.dati.lombardia.it/resource/647i-nhxk.json?idsensore=" + selezione.value  + "&$where=data between" +formatData(dateStart)+ "and" + formatData(dateEnd) + "&$order=data ASC", function (res) {
             var table = document.getElementById("data-table-body");
             var response = JSON.parse(res);
             for (var i = 0; i < response.length; i++) {
@@ -46,31 +48,34 @@ var xhr = new XMLHttpRequest();
 var selezione = document.getElementById("selezione");
 var radios = document.getElementsByName("giorno");
 var data = new Date();
+console.log(formatData(data));
 var dateStart = new Date();
 var dateEnd = new Date();
-dateStart.setDate(dateStart.getDate() - 1);
+
 dateEnd.setDate(dateEnd.getDate() + 1);
-console.log("yesterday-dateStart: ", dateStart);
-console.log("tomorrow-DateEnd: ", dateEnd);
+console.log("yesterday-dateStart: ", formatData(dateStart));
+console.log("tomorrow-DateEnd: ", formatData(dateEnd));
 var dataFormatted = formatData(data);
 
 selezione.addEventListener("change", function(){
-    loadData();
+    loadData(dateStart, dateEnd);
 });
 
 
 for (var i = 0; i < radios.length; i++) {
     radios[i].addEventListener("change", function () {
         if (this.value == "0") {
-            data = new Date();
-            dataFormatted = formatData(data);
-            loadData();
+            dateStart = new Date();
+            dateEnd = new Date();
+            dateEnd.setDate(dateEnd.getDate() + 1);
+            loadData(dateStart, dateEnd);
             console.log(dataFormatted);
         }
         else if (this.value == "-1") {
-            data.setDate(data.getDate() - 1);
-            dataFormatted = formatData(data);
-            loadData();
+            dateStart = new Date();
+            dateStart.setDate(dateStart.getDate()- 1);
+            dateEnd = new Date();
+            loadData(dateStart, dateEnd);
             console.log(dataFormatted);
         }
 
